7da210485c98e8300e47d2e5b72b703d3216c454
Gregory Brown <gregory.t.brown@gmail.com>
2011-09-25 16:52:38 -0400

    Display randomized white rectangles around the map

--- game.rb
+++ game.rb
@@ -7,6 +7,19 @@ Ray.game "Test" do
     @rect = Ray::Polygon.rectangle([-10, -10, 20, 20], Ray::Color.red)
     @rect.pos = [200,200]
 
+    max_x = window.size.width - 20
+    max_y = window.size.height - 20
+
+    @goodies = 20.times.map do 
+      x = rand(max_x) + 10
+      y = rand(max_y) + 10
+      
+      g  = Ray::Polygon.rectangle([0,0,10,10])
+      g.pos = [x,y]
+
+      g
+    end
+
     always do
       @rect.pos += [-2, 0] if holding?(:left)
       @rect.pos += [2, 0] if holding?(:right)
@@ -15,6 +28,7 @@ Ray.game "Test" do
     end
 
     render do |win|
+      @goodies.each { |g| win.draw(g) }
       win.draw @rect
     end
   end
