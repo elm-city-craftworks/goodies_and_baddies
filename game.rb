0d33290f11262177706e253a71ae0a089ec8cabd
Gregory Brown <gregory.t.brown@gmail.com>
2011-09-25 16:22:50 -0400

    Get rectangle to follow mouse

--- game.rb
+++ game.rb
@@ -1,17 +1,20 @@
-# from http://mon-ouie.github.com/projects/ray.html
-
 require 'ray'
+
 Ray.game "Test" do
   register { add_hook :quit, method(:exit!) }
 
   scene :square do
     @rect = Ray::Polygon.rectangle([-10, -10, 20, 20], Ray::Color.red)
-    @rect.pos = [200, 200]
+    @rect.pos = [200,200]
+
+    on :mouse_motion do |pos|
+      @rect.pos = pos
+    end
 
     render do |win|
       win.draw @rect
     end
   end
-
   scenes << :square
 end
+
