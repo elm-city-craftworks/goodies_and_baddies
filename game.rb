125f61516f39f024f550b44aff4c3f15a341dd83
Gregory Brown <gregory.t.brown@gmail.com>
2011-09-25 16:31:59 -0400

    Get rectangle to move left on left key press

--- game.rb
+++ game.rb
@@ -7,14 +7,19 @@ Ray.game "Test" do
     @rect = Ray::Polygon.rectangle([-10, -10, 20, 20], Ray::Color.red)
     @rect.pos = [200,200]
 
-    on :mouse_motion do |pos|
-      @rect.pos = pos
+    on :key_press, key(:left) do 
+      @moving_left = true
+    end
+
+    on :key_release, key(:left) do
+      @moving_left = false
     end
 
     render do |win|
       win.draw @rect
+      @rect.pos += [-1,0] if @moving_left
     end
   end
+
   scenes << :square
 end
-
