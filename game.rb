30bab2c0e6b1c0076c8d348f27186019389050e7
Gregory Brown <gregory.t.brown@gmail.com>
2011-09-25 15:47:41 -0400

    Hello world

---
+++ game.rb
@@ -0,0 +1,11 @@
+# from http://mon-ouie.github.com/projects/ray.html
+
+require 'ray'
+Ray.game "Hello world!" do
+  register { add_hook :quit, method(:exit!) }
+  scene :hello do
+    @text = text "Hello world!", :angle => 30, :at => [100, 100], :size => 30
+    render { |win| win.draw @text }
+  end
+   scenes << :hello
+end
